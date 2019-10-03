68fc73384b0a89a0ddea13b184c16eafe9f9ac6a
hpanditya <hutomo@rubyh.co>
2019-10-03 17:28:33 +0700

    Fix odoo manifest file and remove unnecessary code

--- book/__manifest__.py
+++ book/__manifest__.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "book",
+    "name": "Book",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    "summary": """
+        Module Book for CRUD test from Mplus Software""",
 
-    'description': """
-        Long description of module's purpose
+    "description": """
+        
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    "author": "Hutomo Pebri Anditya",
+    "website": "",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    "category": "Uncategorized",
 
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    "depends": ["base"],
 
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/book.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+
+    "demo": [
+        "demo/demo.xml",
     ],
 }
