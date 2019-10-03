d394d22f4ce4a6e7e55205771718257ee2e1d2c4
hpanditya <hutomo@rubyh.co>
2019-10-03 17:30:13 +0700

    Add new model: Book and Book Type

--- book/models/book.py
+++ book/models/book.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo.datetime import datetime
+from datetime import datetime
 
 
-class book(models.Model):
+class Book(models.Model):
     _name = "book.book"
 
     name = fields.Char(
@@ -17,5 +17,26 @@ class book(models.Model):
         required=True,
     )
 
-    
+    date_published = fields.Date(
+        string="Date Published",
+        defult=fields.Date.today(),
+    )
+
+    number_of_pages = fields.Integer(
+        string="Number of Pages",
+        default=0,
+    )
+
+    book_type_id = fields.Many2one(
+        comodel_name="book.type",
+        string="Type of Book",
+    )
+
+
+class BookType(models.Model):
+    _name = "book.type"
+
+    name = fields.Char(
+        string="Book Type",
+    )
 
